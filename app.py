924e0a2af6a7be1f8be71c8f5b69713b50569487
nhukhanhng <ngkhanh14.kn@gmail.com>
2021-12-29 05:19:15 -0500

    add titles

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import yfinance as yf
 import pandas as pd
 
 st.title("Basic Stock Price App")
-st.write("##### Welcome! Please select a company you want to view the stock prices of. All the data is up-to-date.")
+st.write("##### Welcome! Please select a company you want to view the stock prices of. All the data is up-to-date :)")
 st.text("")
 c1, c2, c3, c4, c5 = st.columns(5)
 
@@ -16,7 +16,9 @@ if c1.button("Google"):
     ticker = "GOOGL"
     google_ticker = yf.Ticker(ticker)
     google_tickerdf = google_ticker.history(period = '5y',  interval = '1d')
+    st.write("#### Closing Price")
     st.line_chart(google_tickerdf.Close)
+    st.write("#### Volume Price")
     st.line_chart(google_tickerdf.Volume)
     remove_unnecessary_column(google_tickerdf)
     
@@ -29,7 +31,9 @@ if c2.button("Coinbase"):
     ticker = "COIN"
     coin_ticker = yf.Ticker(ticker)
     coin_tickerdf = coin_ticker.history(period = '5y',  interval = '1d')
+    st.write("#### Closing Price")
     st.line_chart(coin_tickerdf.Close)
+    st.write("#### Volume Price")
     st.line_chart(coin_tickerdf.Volume)
     remove_unnecessary_column(coin_tickerdf)
     
@@ -40,7 +44,9 @@ if c3.button("Robinhood"):
     ticker = "HOOD"
     hood_ticker = yf.Ticker(ticker)
     hood_tickerdf = hood_ticker.history(period = '5y',  interval = '1d')
+    st.write("#### Closing Price")
     st.line_chart(hood_tickerdf.Close)
+    st.write("#### Volume Price")
     st.line_chart(hood_tickerdf.Volume)
     remove_unnecessary_column(hood_tickerdf)
     
@@ -51,7 +57,9 @@ if c4.button("AMC"):
     ticker = "AMC"
     amc_ticker = yf.Ticker(ticker)
     amc_tickerdf = amc_ticker.history(period = '5y',  interval = '1d')
+    st.write("#### Closing Price")
     st.line_chart(amc_tickerdf.Close)
+    st.write("#### Volume Price")
     st.line_chart(amc_tickerdf.Volume)
     remove_unnecessary_column(amc_tickerdf)
     
@@ -62,7 +70,9 @@ if c5.button("Walt Disney"):
     ticker = "DIS"
     dis_ticker = yf.Ticker(ticker)
     dis_tickerdf = dis_ticker.history(period = '5y',  interval = '1d')
+    st.write("#### Closing Price")
     st.line_chart(dis_tickerdf.Close)
+    st.write("#### Volume Price")
     st.line_chart(dis_tickerdf.Volume)
     remove_unnecessary_column(dis_tickerdf)
     
