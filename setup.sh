24ba76777e4cfe51cfd74c586a8a32a28ab195cb
nhukhanhng <ngkhanh14.kn@gmail.com>
2021-12-29 05:10:48 -0500

    change theme

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
+echo "[theme]
+primaryColor = '#9cb4ff'
+backgroundColor = '#202020'
+secondaryBackgroundColor = '#B9F1C0'
+textColor = '#FFFFFF'
 [server]\n\
 headless = true\n\
 port = $PORT\n\
